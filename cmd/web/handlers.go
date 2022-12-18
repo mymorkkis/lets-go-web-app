4bab5a573216e822a88fe005e65b028f06b20128
mymorkkis <mvsy@protonmail.com>
2022-12-18 20:40:42 +0000

    Chapter 2.7: HTML temlating and inheritence

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
+	"os"
+	"path/filepath"
 	"strconv"
 )
 
@@ -12,7 +16,33 @@ func home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.Write([]byte("Hello from Snippetbox"))
+	basePath, err := getHTMLPath()
+	if err != nil {
+		log.Print(err.Error())
+		errCode := http.StatusInternalServerError
+		http.Error(w, http.StatusText(errCode), errCode)
+	}
+
+	files := []string{
+		filepath.Join(basePath, "base.html"),
+		filepath.Join(basePath, "pages", "home.html"),
+		filepath.Join(basePath, "partials", "nav.html"),
+	}
+
+	templateSet, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Print(err.Error())
+		errCode := http.StatusInternalServerError
+		http.Error(w, http.StatusText(errCode), errCode)
+		return
+	}
+
+	err = templateSet.ExecuteTemplate(w, "base", nil)
+	if err != nil {
+		log.Print(err.Error())
+		errCode := http.StatusInternalServerError
+		http.Error(w, http.StatusText(errCode), errCode)
+	}
 }
 
 func snippetView(w http.ResponseWriter, r *http.Request) {
@@ -34,4 +64,13 @@ func snippetCreate(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Write([]byte("Create a new snippet..."))
+}
+
+func getHTMLPath() (string, error) {
+	wd, err := os.Getwd()
+	if err != nil {
+		return "", err
+	}
+
+	return filepath.Join(wd, "ui", "html"), nil
 }
