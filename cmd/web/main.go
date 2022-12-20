159e70962f28a8984261a7def4ab8e92d857bd17
mymorkkis <mvsy@protonmail.com>
2022-12-20 14:37:55 +0000

    Chapter 4.2 - 4.6: set up first db model

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,15 +1,21 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/mymorkkis/lets-go-web-app/internal/models"
 )
 
 type application struct {
 	errorLog *log.Logger
 	infoLog  *log.Logger
+	snippets *models.SnippetModel
 }
 
 func main() {
@@ -21,9 +27,16 @@ func main() {
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime|log.LUTC)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile|log.LUTC)
 
+	db, err := openDB()
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	defer db.Close()
+
 	app := &application{
 		infoLog:  infoLog,
 		errorLog: errorLog,
+		snippets: &models.SnippetModel{DB: db},
 	}
 
 	routes, err := app.routes()
@@ -40,4 +53,24 @@ func main() {
 	infoLog.Printf("Starting server on %s", *addr)
 	err = server.ListenAndServe()
 	errorLog.Fatal(err)
+}
+
+func openDB() (*sql.DB, error) {
+	// TODO Create db name and port env vars
+	dsn := fmt.Sprintf(
+		"%s:%s@(db:3306)/snippetbox?parseTime=true",
+		os.Getenv("MYSQL_USER"),
+		os.Getenv("MYSQL_PASSWORD"),
+	)
+
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
