cd27a44fc287c1fcb339cb5f1ac81c38645d7929
mymorkkis <mvsy@protonmail.com>
2022-12-20 09:17:26 +0000

    Chapter 3.2: leveled logging

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -14,6 +14,9 @@ func main() {
 	// Could also store multiple settings in a single struct
 	flag.Parse()
 
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime|log.LUTC)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile|log.LUTC)
+
 	mux := http.NewServeMux()
 
 	staticPath, err := getUIPath("static")
@@ -31,9 +34,15 @@ func main() {
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
-	log.Printf("Starting server on %s", *addr)
-	err = http.ListenAndServe(*addr, mux)
-	log.Fatal(err)
+	server := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  mux,
+	}
+
+	infoLog.Printf("Starting server on %s", *addr)
+	err = server.ListenAndServe()
+	errorLog.Fatal(err)
 }
 
 func getUIPath(subfolder string) (string, error) {
