cee32b91764fd0fcac677480ae3096e180e38b00
mymorkkis <mvsy@protonmail.com>
2022-12-23 16:22:00 +0000

    Chapter 11.4: user login

--- internal/models/users.go
+++ internal/models/users.go
@@ -52,7 +52,33 @@ func (m *UserModel) Insert(name, email, password string) error {
 }
 
 func (m *UserModel) Authenticate(email, password string) (int, error) {
-	return 0, nil
+	var (
+		id             int
+		hashedPassword []byte
+	)
+
+	err := m.DB.QueryRow(
+		"SELECT id, hashed_password FROM users WHERE email = ?",
+		email,
+	).Scan(&id, &hashedPassword)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return 0, ErrInvalidCredentials
+		} else {
+			return 0, err
+		}
+	}
+
+	err = bcrypt.CompareHashAndPassword(hashedPassword, []byte(password))
+	if err != nil {
+		if errors.Is(err, bcrypt.ErrMismatchedHashAndPassword) {
+			return 0, ErrInvalidCredentials
+		} else {
+			return 0, err
+		}
+	}
+
+	return id, nil
 }
 
 func (m *UserModel) Exists(id int) (bool, error) {
